7f80625f3d31c499cb5aaeca136390d7bcd6e080
Henri Mattila <henri19102@student.hamk.fi>
2021-06-20 22:06:02 +0300

    minor editing

--- Part3/3.2/python-project/exercise-3-2.py
+++ Part3/3.2/python-project/exercise-3-2.py
@@ -5,17 +5,15 @@ def main():
    repo = "repo"
    git_repository = input("Enter github repository url: ")
    image_name = input("Enter docker image name (username/image_name), which will be pushed to docker hub: ")
-
    git.Repo.clone_from(git_repository, repo)
    os.chdir(os.getcwd() + "/" + repo)
+   find_files("Dockerfile",os.getcwd(), image_name)
 
-   def find_files(filename, path):
-      for root, dir, files in os.walk(path):
-         if filename in files:
-            return os.system(f"docker build . -t {image_name} && docker push {image_name}")
-      return False
-
-   find_files("Dockerfile",os.getcwd())
+def find_files(filename, path, image):
+   for root, dir, files in os.walk(path):
+      if filename in files:
+         return os.system(f"docker build . -t {image} && docker push {image}")
+   return False
 
 if __name__ == '__main__':
     main()
